8af43f242090caa05dfca4ba9c529c7ccdc2c209
riderskart <riderskartdigital@gmail.com>
2025-04-08 16:14:39 +0530

    Delete Admin/Frontend directory

--- Admin/Frontend/public/Notification/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("push", (event) => {
-  const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-//     icon: "/icon.png", // Optional: Path to an icon for the notification
-  });
-});
